db6ae4a5f30622f29c3cf6d7d1331fad68f11569
Mohamad-Gamal <gimephys@gmail.com>
2025-10-20 03:41:07 +0300

    Data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from src.text_summarizer.logging import logger
+from src.textsummarizer.logging import logger
+from src.textsummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("logging is done")
+
+
+
+stage_name = "Data Ingestion Stage"
+
+try:
+    logger.info(f"stage {stage_name} initiated")
+    data_ingestion_pipeline = DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"stage {stage_name} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
